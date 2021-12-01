fa31bb9fcb967f713e11fd6c7eccab8cff9a61c3
Cagdas Ozek <aoez@hellofresh.com>
2021-12-01 15:33:09 +0100

    change payer info type

--- payment_method_nonce.go
+++ payment_method_nonce.go
@@ -8,10 +8,19 @@ type PaymentMethodNonce struct {
 }
 
 type PaymentMethodNonceDetails struct {
-	CardType        string            `xml:"card-type"`
-	Last2           string            `xml:"last-two"`
-	BIN             string            `xml:"bin"`
-	ExpirationMonth string            `xml:"expiration-month"`
-	ExpirationYear  string            `xml:"expiration-year"`
-	PayerInfo       map[string]string `xml:"payer-info"`
+	CardType        string                             `xml:"card-type"`
+	Last2           string                             `xml:"last-two"`
+	BIN             string                             `xml:"bin"`
+	ExpirationMonth string                             `xml:"expiration-month"`
+	ExpirationYear  string                             `xml:"expiration-year"`
+	PayerInfo       PaymentMethodNonceDetailsPayerInfo `xml:"payer-info"`
+}
+type PaymentMethodNonceDetailsPayerInfo struct {
+	Email           string `xml:"email"`
+	FirstName       string `xml:"first-name"`
+	LastName        string `xml:"last-name"`
+	PayerID         string `xml:"payer-id"`
+	CountryCode     string `xml:"country-code"`
+	BillingAddress  string `xml:"billing-address"`
+	ShippingAddress string `xml:"shipping-address"`
 }
