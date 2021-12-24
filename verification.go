1eef6776723885c616f93d4b52a579e629f99620
Cagdas Ozek <aoez@hellofresh.com>
2021-12-24 08:30:21 +0100

    add verification fields

--- verification.go
+++ verification.go
@@ -1,9 +1,20 @@
 package braintree
 
 type Verification struct {
-	XMLName         string `xml:"verification"`
-	CVVResponseCode string `xml:"cvv-response-code,omitempty"`
-	CreatedAt       string `xml:"created-at,omitempty"`
+	XMLName                      string `xml:"verification"`
+	AvsErrorResponseCode         string `xml:"avs-error-response-code"`
+	AvsPostalCodeResponseCode    string `xml:"avs-postal-code-response-code"`
+	AvsStreetAddressResponseCode string `xml:"avs-street-address-response-code"`
+	CurrencyIsoCode              string `xml:"currency-iso-code"`
+	CVVResponseCode              string `xml:"cvv-response-code"`
+	ProcessorResponseCode        string `xml:"processor-response-code"`
+	ProcessorResponseText        string `xml:"processor-response-text"`
+	NetworkResponseCode          string `xml:"network-response-code"`
+	NetworkResponseText          string `xml:"network-response-text"`
+	NetworkTransactionID         string `xml:"network-transaction-id"`
+	MerchantAccountID            string `xml:"merchant-account-id"`
+	GraphqlID                    string `xml:"graphql-id"`
+	ID                           string `xml:"id"`
 }
 
 type Verifications struct {
