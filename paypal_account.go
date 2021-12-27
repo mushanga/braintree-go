c4c2380f82f98a02abe53b8b49a490ba430e433a
Cagdas Ozek <aoez@hellofresh.com>
2021-12-27 15:33:31 +0100

    paypal account details

--- paypal_account.go
+++ paypal_account.go
@@ -6,16 +6,19 @@ import (
 )
 
 type PayPalAccount struct {
-	XMLName       xml.Name              `xml:"paypal-account"`
-	CustomerId    string                `xml:"customer-id,omitempty"`
-	Token         string                `xml:"token,omitempty"`
-	Email         string                `xml:"email,omitempty"`
-	ImageURL      string                `xml:"image-url,omitempty"`
-	CreatedAt     *time.Time            `xml:"created-at,omitempty"`
-	UpdatedAt     *time.Time            `xml:"updated-at,omitempty"`
-	Subscriptions *Subscriptions        `xml:"subscriptions,omitempty"`
-	Default       bool                  `xml:"default,omitempty"`
-	Options       *PayPalAccountOptions `xml:"options,omitempty"`
+	XMLName            xml.Name              `xml:"paypal-account"`
+	CustomerId         string                `xml:"customer-id,omitempty"`
+	Token              string                `xml:"token,omitempty"`
+	Email              string                `xml:"email,omitempty"`
+	ImageURL           string                `xml:"image-url,omitempty"`
+	CreatedAt          *time.Time            `xml:"created-at,omitempty"`
+	UpdatedAt          *time.Time            `xml:"updated-at,omitempty"`
+	Subscriptions      *Subscriptions        `xml:"subscriptions,omitempty"`
+	Default            bool                  `xml:"default,omitempty"`
+	Options            *PayPalAccountOptions `xml:"options,omitempty"`
+	BillingAgreementID string                `xml:"billing-agreement-id,omitempty"`
+	PayerID            string                `xml:"payer-id,omitempty"`
+	RevokedAt          *time.Time            `xml:"revoked-at,omitempty"`
 }
 
 type PayPalAccounts struct {
